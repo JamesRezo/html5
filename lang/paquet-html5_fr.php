d4a60a6fb2e4704efb63a2f676578261cc319252
tetue <tetue@rezo.net>
2019-05-02 01:35:22 +0200

    Description pour paquet.xml

---
+++ lang/paquet-html5_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'html5_description' => 'Ce plugin pose les bases pour faire un site en HTML5. Il fournit actuellement:
+-* un sol commun pratique : reset CSS et un petit fichier javascript pour faire "voir" les balises &#224; Internet Explorer
+-* une surcharge de certains formulaires de SPIP pour utiliser les nouveaux types d\'input',
+	'html5_nom' => 'HTML5 dans SPIP',
+	'html5_slogan' => 'Bases pour faire un site en HTML5'
+);
+
+?>
