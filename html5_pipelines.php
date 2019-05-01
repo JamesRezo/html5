79e814ee2718c6fd8e97305399833d806e10c877
tetue <tetue@rezo.net>
2019-05-02 01:54:58 +0200

    Mieux vaut HTML5 Shiv : https://github.com/aFarkas/html5shiv

--- html5_pipelines.php
+++ html5_pipelines.php
@@ -2,10 +2,11 @@
 
 // Charge la CSS si pas encore fait, par compatibilite avec SPIP 2.0 (pas encore
 // de #INSERT_HEAD_CSS) puis insere un hack javascript pour IE
+// https://github.com/aFarkas/html5shiv
 function html5_insert_head($flux) {
-	$javascript_ie = find_in_path('js/html5.js');
+	$javascript_ie = find_in_path('js/html5shiv.js');
 	$flux .= html5_insert_head_css('')
-		. "<!--[if IE]>\n"
+		. "<!--[if lt IE 9]>\n"
 		. "<script src='$javascript_ie' type='text/javascript'></script>\n"
 		. "<![endif]-->\n";
 	return $flux;
