f5335bddc9e1ede52bada412428bc63d2f9e0bb5
tetue <tetue@rezo.net>
2019-05-02 01:38:08 +0200

    rangement

--- html5_pipelines.php
+++ html5_pipelines.php
@@ -3,7 +3,7 @@
 // Charge la CSS si pas encore fait, par compatibilite avec SPIP 2.0 (pas encore
 // de #INSERT_HEAD_CSS) puis insere un hack javascript pour IE
 function html5_insert_head($flux) {
-	$javascript_ie = find_in_path('javascript/html5-ie.js');
+	$javascript_ie = find_in_path('js/html5.js');
 	$flux .= html5_insert_head_css('')
 		. "<!--[if IE]>\n"
 		. "<script src='$javascript_ie' type='text/javascript'></script>\n"
@@ -14,7 +14,7 @@ function html5_insert_head($flux) {
 function html5_insert_head_css($flux='') {
 	static $html5_headcss_done = false;
 	if (!$html5_headcss_done) {
-		$reset = find_in_path('reset_html5.css');
+		$reset = find_in_path('css/html5.css');
 		$flux .= "<link rel='stylesheet' type='text/css' media='all' href='$reset' />\n";
     $html5_headcss_done = true;
 	}
