93876ccbb6ff382502484c02377201ea1aab4ef0
tetue <tetue@rezo.net>
2019-05-02 13:01:01 +0200

    Debug de syntaxe des fichiers xml SPIP

--- lang/paquet-html5_fr.php
+++ lang/paquet-html5_fr.php
@@ -4,9 +4,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'html5_description' => 'Ce plugin améliore l\'usage de HTML5 dans SPIP en ajoutant :
--* un patch pour émuler les balises HTML5 dans Internet Explorer aussi
--* une surcharge de certains formulaires SPIP',
+	'html5_description' => 'Ce plugin améliore l\'usage de HTML5 dans votre site SPIP en ajoutant :
+-* un patch pour émuler les balises HTML5 dans les vieux Internet Explorer,
+-* une surcharge de certains formulaires SPIP.',
 	'html5_nom' => 'HTML5 dans SPIP',
 	'html5_slogan' => 'Patch pour utiliser HTML5 dans SPIP'
 );
